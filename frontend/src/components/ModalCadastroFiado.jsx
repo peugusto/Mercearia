290e0eebe6492a18ed8f446dfb3596790b979491
Pedro Augusto <pedrooknd@gmail.com>
2025-08-03 22:16:03 -0300

    Update ModalCadastroFiado.jsx
    Terminei a aplicação do react hook form

--- frontend/src/components/ModalCadastroFiado.jsx
+++ frontend/src/components/ModalCadastroFiado.jsx
@@ -1,28 +1,38 @@
 import { useForm } from "react-hook-form";
 import '../css/Modal.css'
 export default function ModalCadastroFiado({closeCadastroFiado}){
-    
+    const {register, handleSubmit} = useForm();
+
+    function onSubmit(data){
+        console.table(data);
+        alert('consulte o console');
+        closeCadastroFiado("exit");
+    }
     return(
         <div className="modal">
             <h1>Cadastro de Fiado</h1>
             
+            <form onSubmit={handleSubmit(onSubmit)}>
             <label>Nome</label>
             <input 
             type="text" 
-            placeholder="Nome..."/>
+            placeholder="Nome..." 
+            {...register("name")}/>
             
             <label>Telefone</label>
             <input 
             type="text"
-            placeholder="Telefone..." />
+            placeholder="Telefone..." 
+            {...register("phone")}/>
 
             <label>Observação</label>
-            <textarea id="obs"></textarea>
+            <textarea id="obs" {...register("phone")}></textarea>
+
             <div className="align">
-            <button onClick={() => closeCadastroFiado("exit")}>Cancelar</button>
-            <button>Cadastrar</button>
+            <button type="button" onClick={() => closeCadastroFiado("exit")}>Cancelar</button>
+            <button type="submit">Cadastrar</button>
             </div>
-            
+            </form>
         </div>
     )
 }   
